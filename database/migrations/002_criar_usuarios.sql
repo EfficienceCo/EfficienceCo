5f1bfc2d20ea37351d4c9b3108c40faaaec6a5c2
Vinicius Carvalho <vinicius.faria@unifesp.br>
2026-04-14 20:21:14 -0300

    feat: adiciona contador de usuarios e chave composta e trigger

--- database/migrations/002_criar_usuarios.sql
+++ database/migrations/002_criar_usuarios.sql
@@ -1,10 +1,13 @@
 -- Migration 002: Criar tabela de usuários
-CREATE TABLE IF NOT EXISTS usuarios (
-  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  cliente_id UUID REFERENCES clientes(id),
-  nome VARCHAR(255) NOT NULL,
-  email VARCHAR(255) UNIQUE NOT NULL,
-  senha_hash TEXT NOT NULL,
-  perfil VARCHAR(20) DEFAULT 'funcionario',
-  criado_em TIMESTAMP DEFAULT NOW()
+CREATE TABLE usuarios (
+    id UUID DEFAULT gen_random_uuid(),
+    cliente_id UUID REFERENCES clientes(id) ON DELETE CASCADE,
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    senha_hash TEXT NOT NULL,
+    perfil VARCHAR(20) DEFAULT 'funcionario',
+    criado_em TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+
+    -- Define a união de ID + CLIENTE_ID como a Identidade Única
+    PRIMARY KEY (id, cliente_id)
 );
